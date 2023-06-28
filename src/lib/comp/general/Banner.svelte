73392496b9842c0211c56cdbb6c2005591adb8cd
AmirSolt <soltamir194@gmail.com>
2023-06-28 17:19:45 -0400

    banner chnage

--- src/lib/comp/general/Banner.svelte
+++ src/lib/comp/general/Banner.svelte
@@ -7,7 +7,7 @@
 			<h1>
 				<span
 					class=" bg-gradient-to-r from-green-500 to-blue-400  box-decoration-clone bg-clip-text text-6xl font-bold text-transparent">
-					Smart Movie Search Engine
+					Movie Search Engine
 				</span>
 			</h1>
 			<br>
